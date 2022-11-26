3bd49e6c1efd56b0ce616f4f61670be72555cd23
Joy Barai <87890046+KIRA-221@users.noreply.github.com>
2022-11-26 15:36:25 +0600

    Add files via upload

---
+++ Assets/Scripts/chikenface.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chikenface : MonoBehaviour {
+
+    private float speed;
+    public Rigidbody2D rb;
+    public WheelJoint2D wj;
+    public GameObject Instantiater;
+	// Use this for initialization
+	void Start ()
+    {
+        speedchange();
+        rb.GetComponents<Rigidbody2D>();
+	}
+
+    void Update()
+    {
+        float totalknifes = Instantiater.GetComponent<Instans>().totalknifes;
+        if(transform.childCount > totalknifes)
+        {
+            Debug.Log ("Game Completed");
+        }
+    }
+	
+	void FixedUpdate()
+    {
+        wj.useMotor = true;
+        JointMotor2D motor = new JointMotor2D { motorSpeed = speed, maxMotorTorque=wj.motor.maxMotorTorque};
+        wj.motor = motor;
+    }
+
+    void speedchange()
+    {
+        speed = Random.Range(-250, 250);
+        StartCoroutine(speedtime());
+    }
+
+    IEnumerator speedtime()
+    {
+        yield return new WaitForSeconds(3f);
+        speedchange();
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag=="Knife")
+        {
+            col.gameObject.tag = "FixedKnife";
+            Destroy(col.attachedRigidbody);
+            col.transform.parent = this.transform;
+        }
+        
+    }
+}
---
+++ Assets/Scripts/knife.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class knife : MonoBehaviour {
+    public Rigidbody2D rb;
+    public float speed = 100f;
+    private bool fire;
+
+	// Use this for initialization
+	void Start () {
+        fire = false;
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0) && fire==false)
+        {
+            hit();
+        }
+	}
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "FixedKnife")
+        {
+            Debug.Log("End Game");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+    }
+
+    void hit()
+    {
+        fire = true;
+        rb.AddForce(Vector2.up * speed, ForceMode2D.Impulse);
+    }
+}
